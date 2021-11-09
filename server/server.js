d55ef01988f8751aba2477b7d749aeb756774e45
Ryan Nguyen <ryannguyen6392@gmail.com>
2021-11-08 22:33:33 -0800

    Added Node API

--- server/server.js
+++ server/server.js
@@ -1,19 +1,28 @@
-const express = require("express");
+ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
 app.use(cors());
-app.use(express.json());
-app.use(require("./routes/record"));
-// get driver connection
-const dbo = require("./db/conn");
- 
-app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
- 
+
+app.use('/login', (req, res) => {
+  res.send({
+    token: 'test123'
   });
-  console.log(`Server is running on port: ${port}`);
 });
+
+app.listen(8080, () => console.log('API is running on http://localhost:8080/login'));
+
+// app.use(express.json());
+// app.use(require("./routes/record"));
+// // get driver connection
+// const dbo = require("./db/conn");
+ 
+// app.listen(port, () => {
+//   // perform a database connection when server starts
+//   dbo.connectToServer(function (err) {
+//     if (err) console.error(err);
+ 
+//   });
+//   console.log(`Server is running on port: ${port}`);
+// });
