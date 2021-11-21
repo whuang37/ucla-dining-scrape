ba4a2aad749915c1fe8107d3d91f30739f3889ed
Tracy Zhao <75452724+TracyZhao24@users.noreply.github.com>
2021-11-21 12:37:23 -0800

    food recommendations container

---
+++ dining-log/src/components/FoodRecs/FoodList.js
@@ -0,0 +1,44 @@
+import React from "react";
+import styled from "styled-components";
+import Food from "./Food";
+
+// get list of all foods from the hall assigned to props.hall
+// display all foods (props.name, props.allergens, props.calories)
+// loop through array of all foods
+
+const Header = styled.div`
+    font-family: Helvetica;
+    text-align: center;
+    font-size: 40px;
+    grid-column: 1 / span 3;
+    justify-self: center;
+`;
+
+const Container = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr; 
+    gap: 5px;
+    justify-content: center;
+    /* width: auto; */
+    border: 1px solid black;
+    margin: 2% 2%;
+    padding-left: 2%;
+    padding-bottom: 2%;
+    padding-top: 1%;
+`;
+
+export default function List(props) {
+    return (
+        <div>
+            <Container>
+                <Header>{props.meal} at {props.hall}</Header>
+                <Food name="Salad" allergens="soy, dairy, nuts" calories="227"/>
+                <Food name="Brown Rice" allergens="gluten" calories="292"/>
+                <Food name="Egg Whites Omelet" allergens="eggs" calories="174"/>
+                <Food name="Sandwich" allergens="gluten" calories="800"/>
+                <Food name="Grilled Chicken" calories="112"/>
+                <Food name="Blueberry Topping" allergens="eggs" calories="41"/>
+            </Container>
+        </div>
+    )
+}
