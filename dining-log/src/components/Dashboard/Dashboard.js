eccac2b3dc2ad6feb9b959cc6c2a5ceec9c72ea2
Maggie Li <maggieelli@ucla.edu>
2021-11-21 11:19:44 -0800

    Edited dashboard page
    Dashboard page previously did not have any content, added it to show the user's profile, including their name, dietary restrictions, and daily calorie goal.

--- dining-log/src/components/Dashboard/Dashboard.js
+++ dining-log/src/components/Dashboard/Dashboard.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import styled from "styled-components";
+import Button from 'react-bootstrap/Button';
 
-export default function Dashboard() {
+const ProfileDiv = styled.div`
+display: grid;
+grid-template-columns: 40% 60%
+`;
+
+const Image = styled.div`
+background-image: ${(props) => props.image};
+background-position: center;
+`;
+
+const TextDiv = styled.div`
+font-family: Helvetica;
+`;
+
+export default function Dashboard(props) {
+  let image = props.image
+  let name = props.name
+  let dietaryRestrictions = props.dietaryRestrictions
+  let calories = props.calories
   return(
-    <h2>Dashboard</h2>
+    <div>
+      <h2>Your Profile</h2>
+      <ProfileDiv> 
+        <Image image = {image}/>
+        <TextDiv>
+          <h3> {name} </h3>
+          <h3> Dietary Restrictions: {dietaryRestrictions} </h3>
+          <h3> Daily Calorie Goal: {calories} </h3>
+        </TextDiv>
+      </ProfileDiv>
+    </div>
   );
 }
