43e919fdd01ea00d3d1e787ca5555aa4f6675e73
Maggie Li <maggieelli@ucla.edu>
2021-11-30 21:41:46 -0800

    Minor edits to navbar
    Fixed name, changed text color to black.

--- dining-log/src/components/navbar.js
+++ dining-log/src/components/navbar.js
@@ -20,10 +20,10 @@ const NavContainer = styled.div`
 export default function NavBar(props) {
     return (
         <NavContainer>
-            <div> <Link to="/" style={{ textDecoration: 'none' }}><strong>UCLA Dining Food Log</strong></Link> </div> 
-            <div> <Link to="/dashboard" style={{ textDecoration: 'none' }}>Your Profile</Link> </div>
-            <div> <Link to="/foodpage" style={{ textDecoration: 'none' }}>Food Logger</Link> </div>
-            <div> <Link to="/history" style={{ textDecoration: 'none' }}>Eating History</Link> </div>
+            <div> <Link to="/" style={{ color: 'black', textDecoration: 'none' }}><strong>UCLA Dining Hall Food Log</strong></Link> </div> 
+            <div> <Link to="/dashboard" style={{ color: 'black', textDecoration: 'none' }}>Your Profile</Link> </div>
+            <div> <Link to="/foodpage" style={{ color: 'black', textDecoration: 'none' }}>Food Logger</Link> </div>
+            <div> <Link to="/history" style={{ color: 'black', textDecoration: 'none' }}>Eating History</Link> </div>
         </NavContainer>
     );
 }
